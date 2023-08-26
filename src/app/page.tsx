9354b5f2d44260c17c290bbddd55ac6e03e48585
SidhantR <sidhantrc02@gmail.com>
2023-08-26 16:20:21 +0530

    Add components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
+import Card from './components/Card'
+import Header from './components/Header'
+import NavBar from './components/NavBar'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <p className="text-red-400">This is from page. tsx</p>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        <NavBar />
+        <main>
+          <Header />
+          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+            <Card />
+          </div>
+        </main>
+      </main>
     </main>
+
   )
 }
---
+++ src/app/search/page.tsx
@@ -0,0 +1,22 @@
+import NavBar from "../components/NavBar";
+import Header from "./components/Header";
+import RestaurantCard from "./components/Restaurant";
+import SideBar from "./components/SideBar";
+
+export default function SearchPage(){
+    return(
+        <main className="bg-gray-100 min-h-screen w-screen">
+        <main className="max-w-screen-2xl m-auto bg-white">
+          <NavBar />
+          <Header />
+          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+            <SideBar />
+            <div className="w-5/6">
+              <RestaurantCard />
+            </div>
+          </div>
+        </main>
+      </main>
+      
+    )
+}
